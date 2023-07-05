32947fd08ed75c4090c7cef4eede2c46147be017
Pankaj <chotuyadav.cy75@gmail.com>
2023-07-05 11:50:43 +0530

    initial commit

---
+++ AngularSpringBootCrud/src/main/java/com/angular/AngularSpringBootCrud/AngularSpringBootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.angular.AngularSpringBootCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringBootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringBootCrudApplication.class, args);
+	}
+
+}
---
+++ AngularSpringBootCrud/src/test/java/com/angular/AngularSpringBootCrud/AngularSpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angular.AngularSpringBootCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringBootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
